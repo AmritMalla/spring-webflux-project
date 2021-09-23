690c2a5a4fec823dc565a6369385c9a49ab36b38
Amrit Malla <amritmalla09@gmail.com>
2021-09-23 18:15:39 +0545

    First commit

---
+++ src/main/java/com/wbc/paa/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.wbc.paa.repository;
+
+import com.wbc.paa.model.User;
+import org.springframework.data.r2dbc.repository.Query;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User,Long> {
+    @Query("select * from users where age >= $1")
+    Flux<User> findByAge(int age);
+
+    Mono<User> findById(int userId);
+}
